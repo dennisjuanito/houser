da255e9075cb59b9ee1897ae1e262c09832b4382
dennis juanito <dennisjuanito@gmail.com>
2018-07-20 09:52:32 -0600

    first  cc

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const massive = require("massive");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+
+
+
+
+
+
+const app = express();
+// top level middle ware
+app.use(bodyParser.json());
+
+
+
+
+// request level middleware
+// endpoints
+ 
+
+
+// make sure to connect to massive first before conencing the app listening
+massive(process.env.CONNECTING_STRING).then(connection => {
+    app.set("db", connection);
+    app.listen(process.env.SERVER_PORT, () => {
+        console.log(`you are in the port${process.env.SERVER_PORT}`)
+    })
+})
+
---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import House from '../House/House.js';
+
+export default class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+};
